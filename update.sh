bd2ccc6decda37a004cc57a41dcd406752c21d61
IljaManakov <32897953+IljaManakov@users.noreply.github.com>
2020-04-25 19:58:04 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python setup.py sdist bdist_wheel
-python -m twine upload dist/*
-rm -r /home/ilja/projects/public/ConvolutionalAutoencoderToolkit/dist
-rm -r /home/ilja/projects/public/ConvolutionalAutoencoderToolkit/build
-rm -r /home/ilja/projects/public/ConvolutionalAutoencoderToolkit/autoencoder.egg-info
